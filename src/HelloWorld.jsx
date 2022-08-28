4bb5edcd8d6eb0e81ece03d354c6e35a620c0593
Mainu <mainu.co.cc@gmail.com>
2022-08-28 10:18:26 +0600

    :sparkles: setup eslint, prettier and husky

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const a = 10;
+
+export default function HelloWorld() {
+  return (
+    <View>
+      <Text>HelloWorld</Text>
+    </View>
+  );
+}
